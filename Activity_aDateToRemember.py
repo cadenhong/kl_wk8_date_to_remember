a00b9a69d76442ec46d523e9454117328240b392
Caden Hong <caden.p.hong@gmail.com>
2022-08-24 14:09:55 -0400

    working script with completed logic and outcomes

--- Activity_aDateToRemember.py
+++ Activity_aDateToRemember.py
@@ -3,20 +3,20 @@
 import random
 
 def separator():
-    print("------------------------------------------------------------")
+    print("----------------------------------------------------------------------")
 
 user = "Caden"
 date = "Casey"
 restaurant = "Kura Diner"
 feelings = ['Uninterested', 'Maybe', 'Interested']
 menu = {'Salads': ['Chicken', 'Green', 'Quinoa'], 'Burgers': ['Classic', 'Veggie', 'Bean'], 'Sides': ['Fries', 'Wings', 'Coleslaw']}
-price = {'Salad': 20, 'Burger': 25, 'Sides': 15}
+price = {'Salads': 20, 'Burgers': 25, 'Sides': 15}
 
 separator()
-print(f'A Date To Remember at {restaurant}'.center(60))
+print(f'A Date To Remember at {restaurant}'.center(70))
 separator()
 print(f'Hi {user} and {date}! Welcome to {restaurant}!\nHope you lovebirds have a great time!\n')
-money = float(input("Enter how much money you brought for tonight: $"))
+money = int(input("Enter how much money you brought for tonight: $"))
 
 if money < 30:
     separator()
@@ -24,33 +24,48 @@ if money < 30:
     separator()
 else:
     separator()
-    print(f'Here is the menu:\n')
+    print(f'{restaurant}\'s Menu\n'.center(70))
     for item in menu:
-        print(f'{item}, ${price[item]}'.center(60))
+        print(f'{item}, ${price[item]}'.center(70))
+    separator()
 
     while True:
         userCategory = input("\nWhich category would you like to choose from? ")
         checkUserInput = menu.get(userCategory.capitalize())
         if checkUserInput is not None:
             while True:
-                userItem = input(f'\nChoose an item from {menu[userCategory]}: ')
+                userCategory = userCategory.capitalize()
+                print(f'From {userCategory} category, select one:', ', '.join(menu[userCategory]))
+                userItem = input('\nWhich item would you like? ')
                 if userItem.capitalize() in menu[userCategory]:
                     userItem = userItem.capitalize()
                     break
                 else:
-                    print("Invalid category!")
+                    print(f'\nInvalid menu item!')
             break
         else:
-            print(f'Invalid category! Select from {menu.keys()}!')
+            print(f'Invalid category! Select from', ', '.join(menu))
 
     dateCategory = random.choice(list(menu.keys()))
     dateRandom = random.randint(0,2)
     dateItem = menu[dateCategory][dateRandom]
 
-    userFeeling = input(f'\nAn order of {userItem} - {userCategory} for {user}, and {dateItem} - {dateCategory} for {date}!\n\n**Whisper**\nSo {user}, how is the date going so far? Choose from {feelings}: ')
+    separator()
+    print("YOUR ORDER".center(70))
+    print(f'{user}: {userItem} ({userCategory})'.center(70))
+    print(f'{date}: {dateItem} ({dateCategory})'.center(70))
+    separator()
     
-    userFeeling = userFeeling.capitalize()
-    dateFeeling = random.choice(feelings)
+    print(f'**Whisper**'.center(70))
+    print(f'So {user}...'.center(70))
+    while True:
+        userFeeling = input(f'\nHow is the date going so far? Choose from {feelings}: ')
+        if userFeeling.capitalize() not in feelings:
+            print("Invalid input!")
+        else:
+            userFeeling = userFeeling.capitalize()
+            dateFeeling = random.choice(feelings)
+            break
 
     separator()
 
@@ -63,18 +78,24 @@ else:
         print(f'\nYour card got declined - there\'s not enough money, the balance is ${money}')
         print(f'{date} - please cover the rest!')
         if dateFeeling == 'Interested':
-            print(f'{date} mentioned that they were {dateFeeling}, but unfortunately, that is not the case anymore...')
+            print(f'\n{date} mentioned that they were {dateFeeling}, but unfortunately, that is not the case anymore...')
             dateFeeling = 'Uninterested'
         print(f'{user}, sorry to break it to you but seems like {date} is not interested in a second date... Good luck!')
+        separator()
     else:
         print(f'\nThanks for the payment, {user}! You now have ${money} left')
+        separator()
+
+        print(f'{user}\'s feeling: {userFeeling}\n{date}\'s feeling: {dateFeeling}\n' )
 
         if userFeeling == 'Uninterested' or dateFeeling == 'Uninterested':
                 print(f'Both of you agreed that this wasn\'t a good match - there won\'t be another date!')
         elif userFeeling == 'Maybe' or dateFeeling == 'Maybe':
             if userCategory == 'Sides' or dateCategory == 'Sides':
-                print("Hmm... Picking a side on a date does not seem too promising... You guys should talk it out and decide on next steps...")
+                print("Hmm... Picking a side on a date doesn't seem too promising but talk it out and decide on next steps.")
             else:
-                print("Forget about feeling unsure! You guys make a good couple, go on another date!")
+                print("Forget about feeling unsure! No one went with a side and from what I saw, you guys make a good couple. Go on another date!")
         else:
-            print('Wooo! Successful date! Definitely set up a second date!')
+            print('Wooo! Who cares about what you ate! You\'re both interested so it\'s a successful date! Definitely set up a second date!')
+
+separator()
