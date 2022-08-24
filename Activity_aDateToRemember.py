d132b7ad099228d8aff8a9d1f081db6a810b9b0f
Caden Hong <caden.p.hong@gmail.com>
2022-08-24 12:09:05 -0400

    Added random module to generate date's input

--- Activity_aDateToRemember.py
+++ Activity_aDateToRemember.py
@@ -1,53 +1,74 @@
 # A Date to Remember!
 
+import random
+
 def separator():
     print("------------------------------------------------------------")
 
-#you = input("Enter your name: ")
-#date = input("Enter your date's name: ")
-caden = "Caden"
-casey = "Casey"
+user = "Caden"
+date = "Casey"
 restaurant = "Kura Diner"
 feelings = ['Bored', 'Uninterested', 'Intrigued', 'Interested']
-menu = {'Salads': ['Grilled Chicken Salad', 'Mixed Baby Greens', 'Quinoa Salad with Salmon'], 'Burgers and Sandwiches': ['Classic Burger', 'Veggie Burger', 'Chicken Sandwich'], 'Sides': ['Fries', 'Wings', 'Coleslaw']}
-price = {'Salads': 20, 'Burgers and Sandwiches': 25, 'Sides': 15}
+menu = {'Salad': ['Chicken', 'Green', 'Quinoa'], 'Burger': ['Classic', 'Veggie', 'Bean'], 'Side': ['Fries', 'Wings', 'Coleslaw']}
+price = {'Salad': 20, 'Burger': 25, 'Side': 15}
 
 separator()
 print(f'A Date To Remember at {restaurant}'.center(60))
 separator()
-print(f'Hi {caden} and {casey}! Welcome to {restaurant}!\nHope you lovebirds have a great time!')
-money = int(input("Please enter how much money you brought for tonight: "))
+print(f'Hi {user} and {date}! Welcome to {restaurant}!\nHope you lovebirds have a great time!\n')
+money = float(input("Enter how much money you brought for tonight: $"))
 
 if money < 30:
     separator()
-    print('Sorry... You don\'t have enough to dine here tonight')
+    print('Sorry, the minimum is $30 - you don\'t have enough to dine here tonight')
+    separator()
 else:
-    print(f'\nHere is the menu:\n')
+    separator()
+    print(f'Here is the menu:\n')
     for item in menu:
-        print(item)
+        print(f'{item}, ${price[item]}'.center(60))
 
-    cadenCategory = input("\nWhich would you like to choose from? ")
-    cadenItem = input(f'Excellent choice! Choose an item from the list: {menu[cadenCategory.capitalize()]}: ')
-    caseyCategory = input(f'\nWhat about you, {casey}? Which would you like to choose from? ')
-    caseyItem = input(f'Awesome! Choose an item from the list: {menu[caseyCategory.capitalize()]}: ')
+    while True:
+        userCategory = input("\nWhich category would you like to choose from? ")
+        checkUserInput = menu.get(userCategory.capitalize())
+        if checkUserInput is not None:
+            while True:
+                userItem = input(f'\nChoose an item from {menu[userCategory.capitalize()]}: ')
+                if userItem in menu[userCategory.capitalize()]:
+                    break
+                else:
+                    print("Invalid category!")
+            break
+        else:
+            print(f'Invalid category! Select from {menu.keys()}!')
 
-    status = input(f'\nAn order of {cadenItem} for {caden}, and {caseyItem} for {casey}! So {caden}, how is the date going so far? Choose from this list: {feelings}: ')
+    dateCategory = random.choice(list(menu.keys()))
+    dateRandom = random.randint(0,2)
+    dateItem = menu[dateCategory][dateRandom]
 
-    payer = input(f'\nOkay, time to pay... Who\'s paying? ')
-    total = price[cadenCategory] + price[caseyCategory]
+    status = input(f'\nAn order of {userItem} for {user}, and {dateItem} for {date}!\n\n*Whisper*So {user}, how is the date going so far? Choose from this list: {feelings}: ')
+
+    separator()
+
+    total = price[userCategory] + price[dateCategory]
     money = money - total
 
-    print(f'{payer} bill came out to ${total} - you now have ${money} left')
+    print(f'Hope you enjoyed your meals. Your total bill comes out to ${total}')
+    
+    if money < 0:
+        print(f'\nYour card got declined - there\'s not enough money, the balance is ${money}')
+    else:
+        print(f'\nThanks for the payment, {user}! You now have ${money} left')
 
-    if status.capitalize() == 'Bored' or status.capitalize() == 'Uninterested':
-        if cadenCategory.capitalize() == 'Sides' or caseyCategory.capitalize() == 'Sides':
-            print('Sorry, there won\'t be a next date...')
-    elif status.capitalize() == 'Intrigued':
-        if cadenCategory.capitalize() == 'Sides' or caseyCategory.capitalize() == 'Sides':
-            print('Sorry, there won\'t be a next date...')
-        elif cadenCategory.capitalize() == 'Salads' or caseyCategory.capitalize() == 'Salads':
-            print('Hmm... Maybe there\'s a chance... Try hitting them up in a couple of days!')
+        if status.capitalize() == 'Bored' or status.capitalize() == 'Uninterested':
+            if userCategory.capitalize() == 'Sides' or dateCategory.capitalize() == 'Sides':
+                print('Sorry, there won\'t be a next date...')
+        elif status.capitalize() == 'Intrigued':
+            if userCategory.capitalize() == 'Sides' or dateCategory.capitalize() == 'Sides':
+                print('Sorry, there won\'t be a next date...')
+            elif userCategory.capitalize() == 'Salads' or dateCategory.capitalize() == 'Salads':
+                print('Hmm... Maybe there\'s a chance... Try hitting them up in a couple of days!')
+            else:
+                print('Wooo! Successful date! Definitely set up a second date!')
         else:
             print('Wooo! Successful date! Definitely set up a second date!')
-    else:
-        print('Wooo! Successful date! Definitely set up a second date!')
